44791e7dae52ea24de88ae77057be5d016c8a8fa
Trevor McCants <trevor.mccants27@gmail.com>
2024-04-19 09:23:37 -0400

    Fixed bug

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,8 @@ def go(args):
     df = df.drop(df[df['price'] > args.max_price].index)
 
     # Save result
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
 
     artifact = wandb.Artifact(
